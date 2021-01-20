2cd65dd7362e069dd94b5f8af6f5498d606b79b8
Jaroslav <j.jacjuk@semrush.com>
2021-01-20 22:57:26 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,6 +1,6 @@
 module "s3-bucket" {
   source  = "app.terraform.io/ErosLove/s3-bucket/aws"
   version = "1.15.0"
-  bucket_prefix = "Jaroslav"
+  bucket_prefix = "Jaroslav Jacjuk"
   # insert required variables here
 }
